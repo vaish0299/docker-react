dc6276faa7a030f2c0c9defa64609d5e31c08a7f
vaish0207 <vaishu0299@gmail.com>
2023-01-09 12:28:10 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Hi i am vaishnavi sai challagulla
+          Edit <code>src/App.js</code> Hi i am Vaishnavi Sai Challagulla
         </p>
         <a
           className="App-link"
